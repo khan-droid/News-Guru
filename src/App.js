d9d8ff7012ac382773c3f7296e7da04d9c156f45
khan-droid <ThisIsKhalidKhan@gmail.com>
2021-09-10 19:52:43 +0530

    Removed page navigation buttons and added infinte scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React, { Component } from 'react'
+import React from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
 import {
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react'
+import InfiniteScroll from "react-infinite-scroll-component";
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
@@ -6,7 +7,15 @@ export default function News(props) {
     const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
-    const [maxPages, setMaxPages] = useState(1)
+    const [totalResults, setTotalResults] = useState(0)
+    const fetchMoreData = async() => {
+        const url = ` https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=330bd9cc3b4a42198fdea0251e43bea6&page=${page+1}`;
+        setPage(page+1) 
+        let data = await fetch(url);
+        let parseData = await data.json();
+        setArticles(articles.concat(parseData.articles))
+        setTotalResults(parseData.totalResults)
+      };
     
     const updateNews = async ()=>{
         const url = ` https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=330bd9cc3b4a42198fdea0251e43bea6&page=${page}`;
@@ -15,50 +24,36 @@ export default function News(props) {
         let parseData = await data.json();
         console.log(parseData);
         setArticles(parseData.articles)
-        setMaxPages(parseData.totalResults)
+        setTotalResults(parseData.totalResults)
         setLoading(false)
     }
     useEffect(() => {
         updateNews();
     },[])
-    const handleNextPage = async ()=>{
-        const url = ` https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=330bd9cc3b4a42198fdea0251e43bea6&page=${page+1}`;
-        setLoading(true);
-        setPage(page+1)
-        let data = await fetch(url);
-        let parseData = await data.json();
-        console.log(parseData);
-        setArticles(parseData.articles)
-        setLoading(false)
-    }
-    const handlePrevPage = async ()=>{
-        const url = ` https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=330bd9cc3b4a42198fdea0251e43bea6&page=${page-1}`;
-        setLoading(true);
-        setPage(page-1)
-        let data = await fetch(url);
-        let parseData = await data.json();
-        console.log(parseData);
-        setArticles(parseData.articles)
-        setLoading(false)
-    }
         return (
-            <div>
+            <>
             <div className="container my-3">
                 <h1 className="text-center" style={{marginTop: '90px'}}>News Guru - Headlines from {(props.category)}</h1>
                 {loading && <Spinner/>}{/*show spinner component only when  loading is true*/}
-                 <div className="row" >
-                {! loading && articles.map((element)=>{
+                
+                <InfiniteScroll
+                    dataLength={articles.length}
+                    next={fetchMoreData}
+                    hasMore={articles.length !== totalResults}
+                    loader={<Spinner/>}
+                >
+                <div className="container">
+                <div className="row" >
+                {articles.map((element)=>{
                 return <div className="col-md-4" key={element.url}>
                     <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} publishedAt={element.publishedAt} author={element.author} />
                 </div>
               })}
               </div>
+              </div>
+              </InfiniteScroll>
             </div>
-            <div className="container d-flex justify-content-between">
-            <button type="button my-3" disabled={ page<=1?true:false} className="btn btn-primary" onClick={handlePrevPage}>Previous</button>
-            <button type="button my-3" disabled={page>=maxPages/20?true:false}className="btn btn-primary" onClick={handleNextPage}>Next</button>
-            </div>
-            </div>
+            </>
         )
 }
 
